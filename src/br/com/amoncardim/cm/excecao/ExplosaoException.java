845253994c501e021c82651f454e4a4a8c3373a3
Amon Cardim <amoncardim@gmail.com>
2023-03-10 08:52:37 -0300

    Primeiro commit:  testes unitários JUnit, parte de abrir o campo minado

--- src/br/com/amoncardim/cm/excecao/ExplosaoException.java
+++ src/br/com/amoncardim/cm/excecao/ExplosaoException.java
@@ -1,2 +1,7 @@
-package br.com.amoncardim.cm.excecao;public class ExplosaoException {
+package br.com.amoncardim.cm.excecao;
+
+public class ExplosaoException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
 }
